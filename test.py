e19e54131ccc01c190ad4925bf5f54617a7cbefc
Andrew White <drewblanco05@gmail.com>
2026-03-19 12:17:09 -0400

    blinking lights :D

--- test.py
+++ test.py
@@ -1,6 +1,2 @@
 # hihihihihihihihi
-<<<<<<< HEAD
 print("Green")
-=======
-print("Hello Heist-it)")
->>>>>>> 4e7c668bb5c93652e6902f890ee0dca40522037f
---
+++ threelight.ino
@@ -0,0 +1,16 @@
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT_PULLUP);
+}
+
+void loop() {
+  int state = digitalRead(9);
+  if (state == LOW){
+    digitalWrite(8, HIGH);
+    delay(1000);
+    digitalWrite(8, LOW);
+    delay(1000);  
+  } 
+  else {
+    digitalWrite(8, LOW);}
+}
